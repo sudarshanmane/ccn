519c6cf09754267ceb50cbe871fe4a5be780de40
sudarshanmane <sudarshanmane2110@gmail.com>
2025-07-16 16:29:51 +0530

    Completed

--- ccn_backend/index.js
+++ ccn_backend/index.js
@@ -3,12 +3,41 @@ import express, { urlencoded } from "express";
 import { createServer } from "http";
 import { StatusCodes } from "http-status-codes";
 import { Server } from "socket.io";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 import connectDB from "./src/config/dbConfig.js";
 import { PORT } from "./src/config/serverConfig.js";
 import apiRouter from "./src/routes/apiRoutes.js";
 
 const app = express();
+
+app.use(helmet());
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  keyGenerator: (req) => {
+    return req?.body?.phone || "";
+  },
+  max: 7,
+  handler: (req, res, next, options) => {
+    const rateLimitLimit = res.getHeader("ratelimit-reset");
+    const callLimit = res.getHeader("ratelimit-limit");
+
+    res.status(429).json({
+      status: "fail",
+      error: "Too Many Requests",
+      message: `You have exceeded the limit of ${callLimit} requests in 15 minutes. Please try again after ${Math.ceil(
+        rateLimitLimit / 60
+      )} minutes.`,
+    });
+  },
+
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use("/api/v1/users/signin", apiLimiter);
 
 app.use(
   cors({
@@ -25,9 +54,6 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  // messageHandlers(io, socket);
-  // messageSocketHandlers(io, socket);
-
   console.log("User connected:", socket.id);
 
   socket.on("joinRoom", (candidateId) => {
